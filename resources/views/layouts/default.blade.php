32b91d629f5d0c2436be678acd3f2226fd4d8ab4
Ericyu65 <nengwuyu@gmail.com>
2020-09-14 13:37:54 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    {{-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
@@ -14,8 +14,16 @@
           <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
         </ul>
       </div>
-    </nav>
+    </nav> --}}
 
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
   </body>
 </html>
